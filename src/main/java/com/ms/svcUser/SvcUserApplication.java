527e4712bec03a3a7b5c232c0c524ae0483d9c3f
edusocrates <edu5390@gmail.com>
2023-10-20 21:59:41 -0300

    Initial commit

---
+++ src/main/java/com/ms/svcUser/SvcUserApplication.java
@@ -0,0 +1,13 @@
+package com.ms.svcUser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SvcUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SvcUserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ms/svcUser/SvcUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ms.svcUser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SvcUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
